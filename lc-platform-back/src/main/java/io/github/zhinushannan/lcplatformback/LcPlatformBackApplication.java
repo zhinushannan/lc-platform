f014c73900d68ff4ac9a1d51b5de4aa96ed44af2
zhinushannan <1377875184@qq.com>
2023-05-29 10:57:44 +0800

    搭建项目的后端基本结构

---
+++ lc-platform-back/src/main/java/io/github/zhinushannan/lcplatformback/LcPlatformBackApplication.java
@@ -0,0 +1,13 @@
+package io.github.zhinushannan.lcplatformback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcPlatformBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcPlatformBackApplication.class, args);
+    }
+
+}
---
+++ lc-platform-back/src/test/java/io/github/zhinushannan/lcplatformback/LcPlatformBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zhinushannan.lcplatformback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcPlatformBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
