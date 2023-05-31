2fc4e1b089d60cb1240be6ea1bb06f3d14860800
zhinushannan <1377875184@qq.com>
2023-05-31 09:27:38 +0800

    增加表逻辑名、业务名合法性检查接口

--- lc-platform-back/src/main/java/io/github/zhinushannan/lcplatformback/service/impl/TableMetaInfoServiceImpl.java
+++ lc-platform-back/src/main/java/io/github/zhinushannan/lcplatformback/service/impl/TableMetaInfoServiceImpl.java
@@ -1,14 +1,16 @@
 package io.github.zhinushannan.lcplatformback.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.github.zhinushannan.lcplatformback.bean.ResultBean;
 import io.github.zhinushannan.lcplatformback.entity.TableMetaInfo;
 import io.github.zhinushannan.lcplatformback.mapper.TableMetaInfoMapper;
 import io.github.zhinushannan.lcplatformback.service.TableMetaInfoService;
+import io.github.zhinushannan.lcplatformback.system.Cache;
 import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author zhinushannan
@@ -17,4 +19,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class TableMetaInfoServiceImpl extends ServiceImpl<TableMetaInfoMapper, TableMetaInfo> implements TableMetaInfoService {
 
+    @Override
+    public ResultBean<String> checkTableLogic(String tableLogicName) {
+        Boolean logicNameExist = Cache.hasTableLogicNameExist(tableLogicName);
+        return logicNameExist ? ResultBean.error("该逻辑表名已存在！") : ResultBean.success();
+    }
+
+    @Override
+    public ResultBean<String> checkTableBusiness(String businessName) {
+        Boolean businessNameExist = Cache.hasTableBusinessNameExist(businessName);
+        return businessNameExist ? ResultBean.error("该业务表名已存在！") : ResultBean.success();
+    }
 }
